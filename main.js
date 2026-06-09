9eda732da452a8be8659d362c949b4cc5a1d41f3
Samukelo khathi <145541253+Samukelokhathi@users.noreply.github.com>
2026-06-09 09:53:41 +0200

    Updated HTML Added form and image

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("Hello World")
+
+const userInput = document.getElementById("email-input")
+const submitBtn = document.getElementById("submit-btn");
+const formElement = document.getElementById("Newsletter-signup-form");
+
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+
+
+    // console.log(userInput)
+
+
+    clearInput()
+})
+
+
+function currentEmail() { return userInput.value.trim() }
+
+// function validateEmail() {
+//     let regExpr = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+//     if (!regExpr.test(email.value)) {
+//         emailError.textContent = "Enter a valid email format"
+//     }
+// }
+
+function clearInput() {
+    userInput.value = ""
+}
+
+
+
+function saveEmail() {
+    let latestEmail = []
+
+}
